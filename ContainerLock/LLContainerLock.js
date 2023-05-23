3ab1580e9106e4051513341544bf80185d36894a
Jacob Malland <62392847+JMalland@users.noreply.github.com>
2023-05-23 12:53:52 -0400

    Update LLContainerLock.js
    Figured out necessary steps to lock containers per user(s).

--- ContainerLock/LLContainerLock.js
+++ ContainerLock/LLContainerLock.js
@@ -4,10 +4,28 @@ function useItemOn(player, item, block) {
     if (block.name != "minecraft:sign") {
         return // Quit the function
     }
-    // Check if hanging, not free-standing
-    // Figure out sign facing using Nbt states
-    // Based on facing, test if block behind is container block
 }
+
+// Need function for user placing a sign --> "onUseItem"
+    // If sign is hanging
+        // Get block behind sign facing
+        // If container behind sign is facing the right way
+            // Parse the sign for '[' + username + ']'
+            // Add NBT tag 'access' to container states, and add username(s) to 'access'
+        // Else, nothing
+    // Else, nothing
+
+// Need function for opening a container --> "onOpenContainer"
+    // If container states contains an 'access' state
+        // If user within 'access', return(true)
+        // Else, return(false)
+    // Else, return(true)
+
+// Need to check if player is destroying a sign. --> "onStartDestroyBlock"
+    // If destroying sign, check if it is on a chest, then check the chest's NBT for 'access'.
+        // If user within 'access', return(true)
+        // Else, return(false)
+    // Else, nothing
 
 function initializeListeners() {
     mc.listen("onUseItemOn", useItemOn) // Listen for players placing blocks
