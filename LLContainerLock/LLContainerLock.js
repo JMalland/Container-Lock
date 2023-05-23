4384ef95d167f4e93e87d6167f84223da44dfda9
Jacob Malland <62392847+JMalland@users.noreply.github.com>
2023-05-23 12:08:20 -0400

    Update LLContainerLock.js
    Created the template for the container lock plugin.

--- LLContainerLock/LLContainerLock.js
+++ LLContainerLock/LLContainerLock.js
@@ -1,1 +1,14 @@
+ll.registerPlugin("Container Lock", "Allow for players to lock chests and other containers using signs.", [1,0,0], {"Author": "JTM"})
 
+function useItemOn(player, item, block) {
+    if (block.name != "minecraft:sign") {
+        return // Quit the function
+    }
+    // Figure out sign facing using Nbt states
+    // Based on facing, test if block behind is container block
+    // Check if hanging, not free-standing
+}
+
+function initializeListeners() {
+    mc.listen("onUseItemOn", useItemOn) // Listen for players placing blocks
+}
