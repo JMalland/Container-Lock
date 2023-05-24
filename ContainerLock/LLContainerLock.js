a20530d02c6ff57080a22d9a2138fb569ea7fb4f
Jacob Malland <62392847+JMalland@users.noreply.github.com>
2023-05-23 21:54:46 -0400

    Need to figure out to store 'access' list in block NBT, preferably in the sign.

--- ContainerLock/LLContainerLock.js
+++ ContainerLock/LLContainerLock.js
@@ -13,25 +13,39 @@ function blockChanged(before, after) {
     if (!after.name.includes("_sign")) { // Sign not being placed
         return // Quit the function
     }
-    log(after.getNbt().toString(1))
-    let nbt = after.getNbt() // Store block nbt
+    let entity_nbt = after.getBlockEntity().getNbt() // Store the block's parent NBT
+    let access = entity_nbt.getTag("FrontText").getTag("Text").toString().split("\n") // List of all players with access to the locked block (must be a container)
     let facing = after.getBlockState().facing_direction // Store the direction the sign is facing
     let target_block = mc.getBlock(compass[facing + (facing%2 == 0 ? 1 : -1)](after.pos)) // Store the block the sign was placed on
-    if (!target_block.hasContainer()) { // Block is not a container
-        log("Isn't Container!")
+    if (entity_nbt.getTag("access") != null) { // The sign already has 'access' players
         return // Quit the function
     }
-    log(after.getBlockEntity().getNbt().toString(1))
-    log(after.getBlockEntity().getNbt().getKeys())
-    log(after.getBlockEntity().getNbt().getTag("FrontText").getKeys())
-    log(after.getBlockEntity().getNbt().getTag("FrontText").getTag("Text"))
+    else if (access[0].toLowerCase() != "[lock]") { // First line references the lock
+        log("Sign not used to lock.")
+        return // Quit the function
+    }
+    else if (!target_block.hasContainer()) { // Block is not a container
+        log("Isn't container, and can't lock a non-container block.")
+        return // Quit the function
+    }
+    for (let i=1; i<access.length; i++) { // Go through each player with access
+        if (mc.getPlayer(access[i]) == null) { // The player listed is invalid
+            log("Player '" + (i + 1) + "' is invalid!")
+            //after.destroy(true) // Destroy the block
+            //return // Quit the function
+        }
+        access[i] = new NbtString(access[i]) // Convert the string into an NbtString
+    }
+    entity_nbt.setTag("access", new NbtList(access.slice(1))) // Create the 'access' tag for the sign, declaring who has access to the container
+    after.getBlockEntity().setNbt(entity_nbt) // Update the sign's NBT
+    log("Created access tag and updated sign NBT.")
 }
 
 function afterPlace(player, block) {
     if (!block.name.includes("_sign")) { // Sign not being placed
         return // Quit the function
     }
-    let facing = block.getBlockState().facing_direction // Store the direction the sign is facing 
+    let facing = block.getBlockState().facing_direction // Store the direction the sign is facing
     let target_block = mc.getBlock(compass[facing + (facing%2 == 0 ? 1 : -1)](block.pos)) // Store the block the sign was placed on
     if (!target_block.hasContainer()) { // Block is not a container
         log("Isn't Container!")
@@ -40,10 +54,28 @@ function afterPlace(player, block) {
     let entity = block.getBlockEntity().getNbt() // Store the entity NBT
     entity.setString("Text", "") // Set the text of the sign, just to trigger the 'blockChanged' function
     block.getBlockEntity().setNbt(entity) // Update the NBT to trigger the 'blockChanged' function
+    log("Updated sign text.")
 }
 
 function openContainer(player, block) {
-
+    log("Opened Container!")
+    let facing = block.getBlockState().facing_direction // Store the direction the sign is facing
+    let target_block = mc.getBlock(compass[facing](block.pos)) // Store the sign in front of the container
+    log(target_block.name)
+    let access_list = target_block.getBlockEntity().getNbt().getTag("access") // Store the block's 'access' list
+    if (access_list != null) { // The sign has the 'access' tag
+        let found = false // Store whether or not the player is within the access list
+        for (let i=0; i<access_list.getSize(); i++) { // Get the users stored in the 'access' list
+            found = found || access_list.getTag(i).toString() == player.name // Determine if player was found
+        }
+        if (!found) { // Player was not found within the list
+            log("You don't have access to this chest!")
+            return(false) // Quit the function
+        }
+    }
+    else {
+        log("No Access List!")
+    }
 }
 
 // Should first break sign, even if destroying the chest
@@ -76,32 +108,8 @@ function destroyContainer(player, block) {
 function initializeListeners() {
     mc.listen("onBlockChanged", blockChanged) // Listen for block change
     mc.listen("afterPlaceBlock", afterPlace) // Listen for block change
-    //mc.listen("onUseItemOn", useItemOn) // Listen for players placing blocks
-    mc.listen("onStartDestroyBlock", (player, block) => { // Verify it is a container block being destroyed
-        let nbt = block.getNbt() // Store block Nbt
-        let state = block.getBlockState() // Store block states
-        let pos = block.pos // Store block position
-        let targets = [] // Store blocks to check
-        if (block.hasContainer()) { // Check it is a container block
-            log("Container: " + JSON.stringify(nbt))
-            targets.push(mc.getBlock(compass[block.getBlockState().facing](pos))) // Add potential sign
-            if (nbt.getTag("pairx") != null) { // Container is paired with a block North/South
-                pos.x = nbt.getTag("pairx") // Update the x value
-                targets.push(mc.getBlock(compass[state.facing](pos))) // Add potential sign
-            }
-            else if (nbt.getTag("pairz") != null) { // Container is paired with a block East/West
-                pos.z = nbt.getTag("pairz") // Update the z value
-                targets.push(mc.getBlock(compass[state.facing](pos))) // Add potential sign
-            }
-            destroyContainer(player, block) // Destroy the container
-        }
-        else if (nbt.getTag("text") != null) { // Check it is a container sign block
-            log("Sign: " + JSON.stringify(nbt))
-        }
-        else {
-            log("Non-Container: " + JSON.stringify(nbt))
-        }
-    })
+    mc.listen("onOpenContainer", openContainer) // Listen for player opening container
+    mc.listen("onStartDestroyBlock", (player, block) => { return })
 }
 
 initializeListeners()
