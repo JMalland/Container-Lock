f8641deee14932a546aab266ec1b7559bf2665cc
Jacob Malland <62392847+JMalland@users.noreply.github.com>
2023-05-25 10:34:33 -0500

    Changed version

--- Container Lock/LLContainerLock.js
+++ Container Lock/LLContainerLock.js
@@ -1,4 +1,4 @@
-ll.registerPlugin("Container Lock", "Allow for players to lock chests and other containers using signs.", [2,1,0], {"Author": "JTM"})
+ll.registerPlugin("Container Lock", "Allow for players to lock chests and other containers using signs.", [2,2,0], {"Author": "JTM"})
 
 var storage = {}
 var config = {}
